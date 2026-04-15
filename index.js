0975975631c5def85e25c36bfdcefffeedabb837
Klaas Stessens <klaas.stessens@student.hogent.be>
2026-04-15 15:05:27 +0200

    feat: function generateStrongPassword done

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-function isStrongPassword(params) {
-    
+function generateStrongPassword(length = 12) {
+ const allowedPasswordCharacters ="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+";
+ const strongPassword = Array.from({ length }, () => {
+ 
+    const randomAllowedPasswordCharacterIndex = Math.floor(Math.random() * allowedPasswordCharacters.length);
+    const randomAllowedPasswordCharacter = allowedPasswordCharacters.charAt(randomAllowedPasswordCharacterIndex);
+    return randomAllowedPasswordCharacter;
+ }).join('');
+ return strongPassword;
 }
+
 
 module.exports = {
     isStrongPassword,
